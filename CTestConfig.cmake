08e1e5eec31ebd0ec3b523a382d303d8c6fb2de4
Andreas Lauser <and@poware.org>
2014-01-24 18:28:01 +0100

    add the files required to submit the testing results to CDash
    see http://opm-project.org/CDash/index.php?project=opm-parser

---
+++ CTestConfig.cmake
@@ -0,0 +1,15 @@
+# this file tells CTest where it should upload its results. To run
+# CTest manually, run
+#
+#  ctest -D Experimental
+#
+
+# in the build directory of the eWoms module. (You must use the CMake
+# build system to make this work.)
+set(CTEST_PROJECT_NAME "opm-parser")
+set(CTEST_NIGHTLY_START_TIME "00:01:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "opm-project.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=opm-parser")
+set(CTEST_DROP_SITE_CDASH TRUE)
---
+++ DartConfiguration.tcl
@@ -0,0 +1,94 @@
+# This file is configured by CMake automatically as DartConfiguration.tcl
+# If you choose not to use CMake, this file may be hand configured, by
+# filling in the required variables.
+
+
+# Configuration directories and files
+SourceDirectory: /home/and/src/opm-parser
+BuildDirectory: /home/and/src/opm-parser
+
+# Where to place the cost data store
+CostDataFile: 
+
+# Site is something like machine.domain, i.e. pragmatic.crd
+Site: heuristix
+
+# Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
+BuildName: Linux-clang++
+
+# Submission information
+IsCDash: TRUE
+CDashVersion: 
+QueryCDashVersion: 
+DropSite: opm-project.org
+DropLocation: /CDash/submit.php?project=opm-parser
+DropSiteUser: 
+DropSitePassword: 
+DropSiteMode: 
+DropMethod: http
+TriggerSite: 
+ScpCommand: /usr/bin/scp
+
+# Dashboard start time
+NightlyStartTime: 01:00:00 UTC
+
+# Commands for the build/test/submit cycle
+ConfigureCommand: "/usr/bin/cmake" "/home/and/src/opm-parser"
+MakeCommand: /usr/bin/gmake -i
+DefaultCTestConfigurationType: Release
+
+# CVS options
+# Default is "-d -P -A"
+CVSCommand: /usr/bin/cvs
+CVSUpdateOptions: -d -A -P
+
+# Subversion options
+SVNCommand: /usr/bin/svn
+SVNOptions: 
+SVNUpdateOptions: 
+
+# Git options
+GITCommand: /usr/bin/git
+GITUpdateOptions: 
+GITUpdateCustom: 
+
+# Generic update command
+UpdateCommand: /usr/bin/git
+UpdateOptions: 
+UpdateType: git
+
+# Compiler info
+Compiler: /usr/bin/clang++
+
+# Dynamic analysis (MemCheck)
+PurifyCommand: 
+ValgrindCommand: 
+ValgrindCommandOptions: 
+MemoryCheckCommand: /usr/bin/valgrind
+MemoryCheckCommandOptions: 
+MemoryCheckSuppressionFile: 
+
+# Coverage
+CoverageCommand: /usr/bin/gcov
+CoverageExtraFlags: -l
+
+# Cluster commands
+SlurmBatchCommand: SLURM_SBATCH_COMMAND-NOTFOUND
+SlurmRunCommand: SLURM_SRUN_COMMAND-NOTFOUND
+
+# Testing options
+# TimeOut is the amount of time in seconds to wait for processes
+# to complete during testing.  After TimeOut seconds, the
+# process will be summarily terminated.
+# Currently set to 25 minutes
+TimeOut: 1500
+
+UseLaunchers: 
+CurlOptions: 
+# warning, if you add new options here that have to do with submit,
+# you have to update cmCTestSubmitCommand.cxx
+
+# For CTest submissions that timeout, these options
+# specify behavior for retrying the submission
+CTestSubmitRetryDelay: 5
+CTestSubmitRetryCount: 3
