a0d13aec9feeed3c188d095657d91cb50182dc21
chflo <chflo@statoil.com>
2015-05-22 16:19:28 +0200

    OPM-199: Fixup in IOConfigTest

--- opm/parser/eclipse/EclipseState/IOConfig/tests/IOConfigTest.cpp
+++ opm/parser/eclipse/EclipseState/IOConfig/tests/IOConfigTest.cpp
@@ -100,6 +100,12 @@ BOOST_AUTO_TEST_CASE(IOConfigTest) {
     std::shared_ptr<const EclipseGrid> grid = std::make_shared<const EclipseGrid>( 10 , 10 , 10 );
     IOConfigPtr ioConfigPtr;
     BOOST_CHECK_NO_THROW(ioConfigPtr = std::make_shared<IOConfig>(deck));
+
+    std::shared_ptr<const GRIDSection> gridSection = std::make_shared<const GRIDSection>(deck);
+    std::shared_ptr<const RUNSPECSection> runspecSection = std::make_shared<const RUNSPECSection>(deck);
+    ioConfigPtr->handleGridSection(gridSection);
+    ioConfigPtr->handleRunspecSection(runspecSection);
+
     Schedule schedule(grid , deck, ioConfigPtr);
 
     //If no BASIC keyord has been handled, no restart files should be written
@@ -258,12 +264,21 @@ BOOST_AUTO_TEST_CASE(IOConfigTest) {
 
     /*Throw exception if write GRID file is specified*/
     DeckPtr deck2 = createDeck(deckStr2);
-    BOOST_CHECK_THROW(std::make_shared<IOConfig>(deck2), std::runtime_error);
+    IOConfigPtr ioConfigPtr2;
+    BOOST_CHECK_NO_THROW(ioConfigPtr2 = std::make_shared<IOConfig>(deck2));
+    std::shared_ptr<const GRIDSection> gridSection2 = std::make_shared<const GRIDSection>(deck2);
+    BOOST_CHECK_THROW(ioConfigPtr2->handleGridSection(gridSection2), std::runtime_error);
 
     /*If NOGGF keyword is present, no EGRID file is written*/
     DeckPtr deck3 = createDeck(deckStr3);
     IOConfigPtr ioConfigPtr3;
     BOOST_CHECK_NO_THROW(ioConfigPtr3 = std::make_shared<IOConfig>(deck3));
+
+    std::shared_ptr<const GRIDSection> gridSection3 = std::make_shared<const GRIDSection>(deck3);
+    std::shared_ptr<const RUNSPECSection> runspecSection3 = std::make_shared<const RUNSPECSection>(deck3);
+    ioConfigPtr3->handleGridSection(gridSection3);
+    ioConfigPtr3->handleRunspecSection(runspecSection3);
+
     BOOST_CHECK_EQUAL(false, ioConfigPtr3->getWriteEGRIDFile());
 
     /*If INIT keyword is specified, verify write of INIT file*/
@@ -279,6 +294,12 @@ BOOST_AUTO_TEST_CASE(IOConfigTest) {
     DeckPtr deck4 = createDeck(deckStr4);
     IOConfigPtr ioConfigPtr4;
     BOOST_CHECK_NO_THROW(ioConfigPtr4 = std::make_shared<IOConfig>(deck4));
+
+    std::shared_ptr<const GRIDSection> gridSection4 = std::make_shared<const GRIDSection>(deck4);
+    std::shared_ptr<const RUNSPECSection> runspecSection4 = std::make_shared<const RUNSPECSection>(deck4);
+    ioConfigPtr4->handleGridSection(gridSection4);
+    ioConfigPtr4->handleRunspecSection(runspecSection4);
+
     BOOST_CHECK_EQUAL(false, ioConfigPtr4->getWriteEGRIDFile());
 }
 
