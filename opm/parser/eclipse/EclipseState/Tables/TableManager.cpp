2ca8f253fe8cc2f63d804c93619a89333eaefbf5
Pål Grønås Drange <PGDR@statoil.com>
2016-10-31 14:55:34 +0100

    Minor fixes unrelated to PR:
    * Implemented missing method "name" in TableColumn.cpp
    * add inline to method in RestartConfig to remove warning
    * Whitespace changes

--- opm/parser/eclipse/EclipseState/Tables/TableManager.cpp
+++ opm/parser/eclipse/EclipseState/Tables/TableManager.cpp
@@ -232,9 +232,9 @@ namespace Opm {
             }
             addTables( "SORWMIS", numMiscibleTables);
             addTables( "SGCWMIS", numMiscibleTables);
-            addTables( "MISC", numMiscibleTables);
-            addTables( "PMISC", numMiscibleTables);
-            addTables( "TLPMIXPA", numMiscibleTables);
+            addTables( "MISC",    numMiscibleTables);
+            addTables( "PMISC",   numMiscibleTables);
+            addTables( "TLPMIXPA",numMiscibleTables);
         }
 
         {
